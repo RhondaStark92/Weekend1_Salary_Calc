c72290f1caed8200c8d0dcbd713f07722ea7c06d
Rhonda Stark <rhondastark@Rhondas-MacBook-Pro.local>
2018-10-06 16:37:16 -0500

    updated table to use all the fields

--- client.js
+++ client.js
@@ -6,12 +6,17 @@ class Employee{
     this.jobTitle = jobTitle;
     this.annualSalary = annualSalary;
   } // end constructor
+
+  // function to calculate monthly salary
+  monthlySalary() {
+  } // end monthlySalary function
+
 } // end Employee class
 
 // employees array
 let employeesArray = [];
 
-// add employee from input to the array
+// function to add employee from input to the array
 function addToArray (){
   // retrieve input from the DOM
   let inFirstName = $( '#firstNameIn' ).val();
@@ -29,18 +34,20 @@ function addToArray (){
   console.log(employeesArray);
 } // end addToArray function
 
+// function to update the employee table on the DOM
 function displayEmployees() {
-  // update list of employees
-  let outputList = $( '#outputList');
-  outputList.empty();
-  // Loop through the employees array
+  // retrieve the table from the DOM and setup the header row
   let tableDocument = $( '#tableEmployees' );
-  let tableRow = `<tr><th>Firstname</th><th>Lastname</th><th>Age</th></tr>`;
+  let tableRow = `<tr><th>First Name</th><th>Last Name</th><th>ID</th>
+    <th>Title</th><th>Annual Salary</th></tr>`;
+  // clear the table and add the header row
   tableDocument.empty();
   tableDocument.append(tableRow);
+  // loop through the array of employees
   for (person of employeesArray) {
     tableRow = '<tr><td>' + person.firstName + '</td><td>' + 
-      person.LastName + '</td><td>' + person.idNum + '</td>' +
+      person.LastName + '</td><td>' + person.idNum + '</td><td>' + 
+      person.jobTitle + '</td><td>' + person.annualSalary + '</td>' +
       '</tr>';
     console.log(tableRow);
     tableDocument.append(tableRow);
@@ -52,18 +59,15 @@ function addEmployee () {
   // get input fields
   event.preventDefault();
   console.log(' button clicked ');
-
+  // call to add to the array
   addToArray();
-
   // Clear fields after retreiving them
   $("#inputForm").trigger('reset');
-
   // Add to the DOM in list at first
   displayEmployees();
-  
 } // end addEmployee function
 
-
+// DOM loaded
 $( document ).ready ( readyNow );
 
 // function once DOM is loaded and ready
